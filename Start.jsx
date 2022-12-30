591bb53e35bf5b3768946c3dd98a0fce7488d992
Mark Aloo <mark@faru.health>
2022-12-30 21:14:23 +0300

    Initialize project using Create React App

---
+++ Start.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Start = () => {
+  return (
+    <div className='page'>
+      <div className='container'>
+        <div className='header'>
+          <div className='navbar'></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
