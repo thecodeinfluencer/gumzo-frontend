c6c4ee2842343dcf13442ccba9a6d3d8d746d609
Mark Aloo <mark@faru.health>
2023-01-11 17:12:52 +0300

    add api

---
+++ src/api/apiSlice.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// const apiUrl = 'http://localhost:5000/api/v1/client';
+// const apiUrl = 'http://104.248.199.100/api/v1/client';
+const apiUrl = 'https://gumzoai.el.r.appspot.com//api/v1/client';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: apiUrl,
+    prepareHeaders: (headers, { getState }) => {
+      // headers.set('accept', 'application/json');
+      // const token = getState().auth.token;
+      // token && headers.set('authorization', `Bearer ${token}`);
+      return headers;
+    },
+  }),
+  endpoints: builder => ({
+    getListData: builder.query({
+      query: query => query,
+    }),
+    postListData: builder.mutation({
+      query: ({ ...rest }) => ({ method: 'POST', ...rest }),
+    }),
+    putListData: builder.mutation({
+      query: ({ ...rest }) => ({ method: 'PUT', ...rest }),
+    }),
+    deleteListData: builder.mutation({
+      query: url => ({ url, method: 'DELETE' }),
+    }),
+  }),
+});
+
+export const {
+  useGetListDataQuery,
+  usePostListDataMutation,
+  usePutListDataMutation,
+  useDeleteListDataMutation,
+} = apiSlice;
