cdaab565b7aa06d97b2972912f5abad2179cf371
Mark Aloo <mark@faru.health>
2023-01-11 17:12:01 +0300

    add formik

---
+++ src/formik/FormikInput.jsx
@@ -0,0 +1,26 @@
+import { TextField } from '@mui/material';
+import { useFormikContext } from 'formik';
+import React from 'react';
+
+export default function FormikInput({ name, type, onSelectFiles, ...other }) {
+  const { errors, setFieldTouched, setFieldValue, touched, values } =
+    useFormikContext();
+
+  return (
+    <TextField
+      fullWidth
+      variant='outlined'
+      type={type}
+      style={{ marginTop: 8, marginBottom: 8 }}
+      error={Boolean(name && touched[name] && errors[name])}
+      value={name && values[name]}
+      onChange={e => {
+        name && setFieldValue(name, e.target.value);
+        type == 'file' && onSelectFiles(e.target.files);
+      }}
+      onBlur={() => name && setFieldTouched(name)}
+      helperText={!!(name && touched[name] && errors[name]) && errors[name]}
+      {...other}
+    />
+  );
+}
