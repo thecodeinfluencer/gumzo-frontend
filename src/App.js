5dc31120789c3d64bccd67d537aa5f7b3c0929b1
Mark Aloo <mark@faru.health>
2023-01-11 17:14:25 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { apiSlice } from './api/apiSlice';
+import { useAuth } from './firebase/auth';
+import Dashboard from './pages/Dashboard';
+import Landing from './pages/Landing';
+import SignIn from './pages/SignIn';
+import messagesReducer from './redux/slices/messagesSlice';
+import { loadFromLocalStorage, saveToLocalStorage } from './redux/store';
 
 function App() {
+  const { authUser } = useAuth();
+  const persistedStorage = loadFromLocalStorage(authUser?.uid);
+
+  const store = configureStore({
+    preloadedState: persistedStorage,
+    reducer: {
+      [apiSlice.reducerPath]: apiSlice.reducer,
+      messages: messagesReducer,
+    },
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({ serializableCheck: false }).concat(
+        apiSlice.middleware
+      ),
+  });
+
+  store.subscribe(() => saveToLocalStorage(store.getState(), authUser?.uid));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/auth' element={<SignIn />} />
+          <Route path='/chat' element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
